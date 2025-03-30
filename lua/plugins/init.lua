59605ddb001cd14e8f695f904845252064bc3a0c
tomurb <tomasz@flysmarter.dk>
2025-03-30 18:51:47 +0200

    add treesitter-textobjects

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -304,8 +304,11 @@ return {
       --
       --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
+  },
+
+  { -- adds new textobjects, like function, class etc.
+    'nvim-treesitter/nvim-treesitter-textobjects',
   },
 
   { -- shows in which function you are
