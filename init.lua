606e6e8bee688f19d2454bee2c31da967563177f
tomurb <tomasz@flysmarter.dk>
2025-04-05 12:32:14 +0200

    exit terminal mode C-/

--- init.lua
+++ init.lua
@@ -180,7 +180,7 @@ vim.o.expandtab = true
 -- --
 -- -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- -- or just use <C-\><C-n> to exit terminal mode
--- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('t', '<C-/>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 --
 -- -- TIP: Disable arrow keys in normal mode
 -- -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
