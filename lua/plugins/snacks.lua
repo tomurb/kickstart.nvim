745ca631df18c29f4aed0b9be67d3d1ed34daa70
tomurb <tomasz@flysmarter.dk>
2025-10-21 16:06:10 +0200

    Turn off snacks/explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,7 +4,7 @@ return {
   opts = {
     bigfile = { enabled = true },
     dashboard = { enabled = true },
-    explorer = { enabled = true },
+    explorer = { enabled = false },
     indent = { enabled = true },
     input = { enabled = true },
     picker = { enabled = false },
